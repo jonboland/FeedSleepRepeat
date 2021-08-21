624ea5067e11baeb5d7b7f8d1a316d1efe7ca124
jonboland <jon.boland@gmail.com>
2021-08-21 08:41:38 +0100

    Used partial class to segment out helper methods and property settings

--- FeedSleepRepeatUI/FeedSleepRepeat.cs
+++ FeedSleepRepeatUI/FeedSleepRepeat.cs
@@ -19,10 +19,9 @@ namespace FeedSleepRepeatUI
         public FeedForm()
         {
             InitializeComponent();
+            ApplyPropertySettings();
             LoadBabyList();
             ConnectBabyList();
-            dateOfBirthPicker.MaxDate = DateTime.Today;
-            datePicker.MaxDate = DateTime.Today;
         }
 
         private void babyNameCombo_SelectedIndexChanged(object sender, EventArgs e)
@@ -35,7 +34,7 @@ namespace FeedSleepRepeatUI
             }
             else
             {
-                Baby CurrentBaby = babySearch.First();
+                CurrentBaby = babySearch.First();
                 dateOfBirthPicker.Value = CurrentBaby.DateOfBirth;
                 ageBox.Text = CalculateAge(CurrentBaby.DateOfBirth);
                 nappiesTotal.Text = UpdateTotalNappies();
@@ -91,72 +90,6 @@ namespace FeedSleepRepeatUI
             ResetValues();
             LoadBabyList();
             ConnectBabyList();
-        }
-
-        private void ResetValues()
-        {
-            CurrentBaby = null;
-            dateOfBirthPicker.Value = DateTime.Today.Date;
-            ageBox.Text = "0y 0m 0d";
-            wetNappiesNumericUpDown.Value = 0;
-            dirtyNappiesNumericUpDown.Value = 0;
-            nappiesTotal.Text = "0";
-        }
-
-        private void LoadBabyList()
-        {
-            Babies = SqliteDataAccess.LoadBabies();
-            Babies.Insert(0, new Baby()
-            {
-                FirstName = String.Empty,
-                LastName = String.Empty,
-                DateOfBirth = DateTime.Today.Date
-            });
-        }
-
-        private void ConnectBabyList()
-        {
-            babyNameCombo.DataSource = null;
-            babyNameCombo.DataSource = Babies;
-            babyNameCombo.DisplayMember = "FullName";
-        }
-
-        private Baby GenerateBabyInstance()
-        {
-            Baby b = new();
-
-            string[] name = babyNameCombo.Text.Split();
-
-            b.FirstName = name[0];
-            b.LastName = name[1];
-            b.DateOfBirth = dateOfBirthPicker.Value.Date;
-
-            return b;
-        }
-
-        private string CalculateAge(DateTime dateOfBirth)
-        {
-            TimeSpan timeSpan = DateTime.Now.Subtract(dateOfBirth);
-            DateTime age = DateTime.MinValue + timeSpan;
-            return $"{age.Year - 1}y {age.Month - 1}m {age.Day - 1}d";
-        }
-
-        private BabyDay GenerateBabyDayInstance()
-        {
-            BabyDay d = new();
-
-            d.Date = datePicker.Value;
-            d.Weight = weightBox.Text;
-            d.WetNappies = wetNappiesNumericUpDown.Value;
-            d.DirtyNappies = dirtyNappiesNumericUpDown.Value;
-
-            return d;
-        }
-
-        private string UpdateTotalNappies()
-        {
-            decimal totalNappies = wetNappiesNumericUpDown.Value + dirtyNappiesNumericUpDown.Value;
-            return totalNappies.ToString();
         }
     }
 }
