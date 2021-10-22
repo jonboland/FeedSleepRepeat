c15ee9b8b225d9ec98a7e566e9f4b7525f2d52a0
jonboland <jon.boland@gmail.com>
2021-10-22 16:17:07 +0100

    Added ThreadExceptionEventHandler to handle fatal errors gracefully

--- FeedSleepRepeatUI/Program.cs
+++ FeedSleepRepeatUI/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +15,26 @@ namespace FeedSleepRepeatUI
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FeedForm());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            // TODO: Add exception logging
+            
+            MessageBox.Show(
+                "Sorry for the disruption.\n\n"
+                + "Unfortunately, FeedSleepRepeat has stopped working because the following fatal error occured:\n\n" 
+                + e.Exception.Message,
+                "Fatal Error", 
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Stop);
+
+            Application.Exit();
         }
     }
 }
