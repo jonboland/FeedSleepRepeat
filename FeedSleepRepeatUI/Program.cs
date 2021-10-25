b9c37c3d258fbb4645e99140b8d4829a1ab03013
jonboland <jon.boland@gmail.com>
2021-10-25 15:19:40 +0100

    Moved db to DataDir to prevent ClickOnce overwrites

--- FeedSleepRepeatUI/Program.cs
+++ FeedSleepRepeatUI/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace FeedSleepRepeatUI
 {
     static class Program
     {
+        static readonly string LocalAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        static readonly string DataDir = Path.Combine(LocalAppData, "FSR");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -19,22 +23,53 @@ namespace FeedSleepRepeatUI
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            CreateDataDirIfDoesNotExist();
+            CopyDatabaseToDataDirIfDoesNotExist();
+            AppDomain.CurrentDomain.SetData("DataDirectory", DataDir);
             Application.Run(new FeedForm());
         }
 
+        /// <summary>
+        /// Shows message box id fatal error occurs and exits application when user clicks OK.
+        /// </summary>
         static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
         {
             // TODO: Add exception logging
-            
+
             MessageBox.Show(
                 "Sorry for the disruption.\n\n"
-                + "Unfortunately, FeedSleepRepeat has stopped working because the following fatal error occured:\n\n" 
+                + "Unfortunately, FeedSleepRepeat has stopped working because the following fatal error occured:\n\n"
                 + e.Exception.Message,
-                "Fatal Error", 
-                MessageBoxButtons.OK, 
+                "Fatal Error",
+                MessageBoxButtons.OK,
                 MessageBoxIcon.Stop);
 
             Application.Exit();
+        }
+
+        /// <summary>
+        /// Creates the DataDir folder in AppData/Local if it doesn't already exist.
+        /// </summary>
+        static void CreateDataDirIfDoesNotExist()
+        {
+            if (!Directory.Exists(DataDir))
+            {
+                Directory.CreateDirectory(DataDir);
+            }
+        }
+
+        /// <summary>
+        /// Copies the database file from the deployment location to the DataDir folder if it doesn't already exist.
+        /// </summary>
+        static void CopyDatabaseToDataDirIfDoesNotExist()
+        {
+            string sourceFilePath = Path.Combine(Application.StartupPath, "FeedSleepRepeatDB.db");
+            string destFilePath = Path.Combine(DataDir, "FeedSleepRepeatDB.db");
+
+            if (!File.Exists(destFilePath))
+            {
+                File.Copy(sourceFilePath, destFilePath);
+            }
         }
     }
 }
