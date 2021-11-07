2471eab1fbe797e8c6bc6923abc3873478161dc8
jonboland <jon.boland@gmail.com>
2021-11-07 16:19:47 +0000

    Added weight chart methods

--- FeedSleepRepeatUI/WeightChart.Designer.cs
+++ FeedSleepRepeatUI/WeightChart.Designer.cs
@@ -29,12 +29,55 @@ namespace FeedSleepRepeatUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.weightsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.weightsChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // weightsChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.weightsChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.weightsChart.Legends.Add(legend1);
+            this.weightsChart.Location = new System.Drawing.Point(12, 12);
+            this.weightsChart.Name = "weightsChart";
+            this.weightsChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.weightsChart.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.MediumSeaGreen};
+            series1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.EmptyPointStyle.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series1.EmptyPointStyle.Color = System.Drawing.Color.MediumSeaGreen;
+            series1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.Legend = "Legend1";
+            series1.Name = "Weights";
+            series1.YValuesPerPoint = 2;
+            this.weightsChart.Series.Add(series1);
+            this.weightsChart.Size = new System.Drawing.Size(776, 426);
+            this.weightsChart.TabIndex = 0;
+            this.weightsChart.Text = "weightChart";
+            // 
+            // WeightChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "WeightChart";
+            this.Controls.Add(this.weightsChart);
+            this.Name = "WeightChart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weight Chart";
+            this.Load += new System.EventHandler(this.WeightChart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.weightsChart)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart weightsChart;
     }
 }
--- FeedSleepRepeatUI/WeightChart.cs
+++ FeedSleepRepeatUI/WeightChart.cs
@@ -7,18 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using FeedSleepRepeatLibrary;
 
 namespace FeedSleepRepeatUI
 {
     public partial class WeightChart : Form
     {
-        public Baby CurrentBaby { get; set; }
+        private readonly Baby currentBaby;
 
-        public WeightChart(Baby currentBaby)
+        public WeightChart(Baby baby)
         {
             InitializeComponent();
-            CurrentBaby = currentBaby;
+            SetEmptyPointStyle();
+            currentBaby = baby;
+        }
+
+        private void WeightChart_Load(object sender, EventArgs e)
+        {
+            FillChart();
+        }
+
+        private void FillChart()
+        {
+            DateTime earliestDay = currentBaby.BabyDays.Min(d => d.Date);
+            DateTime latestDay = currentBaby.BabyDays.Max(d => d.Date);
+            double totalDays = (latestDay - earliestDay).TotalDays;
+
+
+            for (int i = 0; i <= totalDays; i++)
+            {
+                double weight = double.NaN;
+                DateTime date = earliestDay.AddDays(i);
+                BabyDay day = currentBaby.BabyDays.FirstOrDefault(d => d.Date == date);
+
+                if (day != null && day.Weight != String.Empty)
+                {
+                    weight = Convert.ToDouble(day.Weight);
+                }
+
+                weightsChart.Series["Weights"].Points.AddXY(date, weight);
+            }
+        }
+
+        private void SetEmptyPointStyle()
+        {
+            weightsChart.Series["Weights"].EmptyPointStyle.BorderDashStyle = ChartDashStyle.Dash;
+            weightsChart.Series["Weights"].CustomProperties = "EmptyPointValue = Average";
         }
     }
 }
