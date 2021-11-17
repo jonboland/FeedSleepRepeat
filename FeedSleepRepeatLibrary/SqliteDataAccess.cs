01f9f74575cb54cec4eb978453c356634e7aa0b9
jonboland <jon.boland@gmail.com>
2021-11-17 16:50:44 +0000

    Removed unneeded using statements from all files

--- FeedSleepRepeatLibrary/SqliteDataAccess.cs
+++ FeedSleepRepeatLibrary/SqliteDataAccess.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SQLite;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Dapper;
 
 namespace FeedSleepRepeatLibrary
